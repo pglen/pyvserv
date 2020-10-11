4c0f0130db6688b592b56f40dda9a83253804822
Peter <peterglen99@gmail.com>
2020-10-11 16:53:57 -0400

    auto

--- common/pypacker.py
+++ common/pypacker.py
@@ -23,7 +23,8 @@ Int Number            i         Float Number          f
 Character             c         String                s
 Binary                b
 Extended              x         encoded as new packer string
-List                  t         gets encoded as extended
+List                  l         gets encoded as extended
+Tuple                 t         gets encoded as extended
 
 use: pb  = packbin(); newdata = pb.encode_data(formatstr, arr_of_data)
 orgdata  = pb.decode_data(newdata)
@@ -54,63 +55,76 @@ class packbin():
         # extended list
 
         self.typeact = [
-                 ["i", self._got_int, self._found_int],
-                 ["l", self._got_long, self._found_long],
-                 ["f", self._got_float, self._found_float],
-                 ["c", self._got_char, self._found_char],
-                 ["s", self._got_str, self._found_str],
-                 ["b", self._got_bin, self._found_bin],
-                 ["t", self._got_list, self._found_list],
-                 ["x", self._got_xtend, self._found_ext],
-
-                 # No dictionary support (for now)
-                 #["d", self._got_dict, self._found_dict],
+                 ["i", self.__got_int, self.__found_int],
+                 ["l", self.__got_long, self.__found_long],
+                 ["f", self.__got_float, self.__found_float],
+                 ["c", self.__got_char, self.__found_char],
+                 ["s", self.__got_str, self.__found_str],
+                 ["b", self.__got_bin, self.__found_bin],
+                 ["a", self.__got_list, self.__found_list],
+                 ["t", self.__got_tuple, self.__found_tuple],
+                 ["x", self.__got_xtend, self.__found_ext],
+                 ["d", self.__got_dict, self.__found_dict],
                 ]
 
         self.verbose = 0
 
     # These functions contain the ations on encode
 
-    def _got_int(self, tt, var):
+    def __got_int(self, tt, var):
         #print ("got int", var)
         return "%c%d %d " %  (tt, 4, var)
 
-    def _got_long(self, tt, var):
+    def __got_long(self, tt, var):
         #print ("got long", var)
         return "%c%d %d " %  (tt, 8, var)
 
-    def _got_float(self, tt, var):
+    def __got_float(self, tt, var):
         #print ("got num", "'" + str(var) + "'")
         return "%c%d %f " %  (tt, 8, var)
 
-    def _got_char(self, tt, var):
+    def __got_char(self, tt, var):
         #print ("got char", "'" + str(var) + "'")
         return "%c%d %c " %  (tt, 1, var)
 
-    def _got_str(self, tt, var):
+    def __got_str(self, tt, var):
         #print ("got str", "'" + var + "'")
         return "%c%d '%s' " %  (tt, len(var), var)
 
-    def _got_bin(self, tt, var):
+    def __got_bin(self, tt, var):
         enco    = base64.b64encode(var)
         if sys.version_info[0] > 2:
             enco  = enco.decode("cp437")
         #print ("got bin", "'" + enco + "'")
         return "%c%d '%s' " %  (tt, len(enco), enco)
 
-    def _got_list(self, tt, var):
+    def __got_list(self, tt, var):
         #print ("got list", "'" + str(var) + "'")
         enco = self.encode_data("", *var)
         return "%c%d '%s' " %  (tt, len(enco), enco)
 
-    def _got_xtend(self, tt, var):
+    def __got_tuple(self, tt, var):
+        #print ("got tuple", "'" + str(var) + "'")
+        enco = self.encode_data("", *var)
+        return "%c%d '%s' " %  (tt, len(enco), enco)
+
+    def __got_xtend(self, tt, var):
         #print ("got xtend", "'" + str(var) + "'")
         return "%c%d [%s] " %  (tt, len(var), var)
+
+    def __got_dict(self, tt, var):
+        #print ("got dict", "'" + str(var) + "'")
+        # Flatten it
+        ccc = []
+        for aa in var:
+            ccc.append((aa, var[aa]))
+        sss = self.encode_data("", ccc)
+        return "%c%d '%s' " %  (tt, len(sss), sss)
 
     # ------------------------------------------------------------------------
     # Return var and consumed number of characters
 
-    def _found_char(self, xstr):
+    def __found_char(self, xstr):
         idxx = 0; var = 0
         #print ("found int:", xstr)
         if xstr[1:3] != "1 ":
@@ -129,7 +143,7 @@ class packbin():
         #print("int idxx:", idxx, "var:", var, "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, chr(var)
 
-    def _found_int(self, xstr):
+    def __found_int(self, xstr):
         idxx = 0; var = 0
         #print ("found int:", xstr)
         if xstr[1:3] != "4 ":
@@ -144,7 +158,7 @@ class packbin():
         #print("int idxx:", idxx, "var:", var, "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, var
 
-    def _found_long(self, xstr):
+    def __found_long(self, xstr):
         idxx = 0; var = 0
         #print ("found long:", xstr)
         if xstr[1:3] != "8 ":
@@ -159,7 +173,7 @@ class packbin():
         #print("long idxx:", idxx, "var:", var, "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, var
 
-    def _found_float(self, xstr):
+    def __found_float(self, xstr):
         idxx = 0; var = 0
         #print ("found long:", xstr)
         if xstr[1:3] != "8 ":
@@ -174,7 +188,7 @@ class packbin():
         #print("float idxx:", idxx, "var:", var, "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, var
 
-    def _found_str(self, xstr):
+    def __found_str(self, xstr):
         idxx = 0
         #print ("found str:", xstr)
         idxx = 1
@@ -192,7 +206,31 @@ class packbin():
         #print("idxx:", idxx, "var:", "{" + sval + "}", "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, sval
 
-    def _found_ext(self, xstr):
+    def __found_dict(self, xstr):
+        idxx =  0
+        #print ("found dict:", xstr)
+        idxx =  1
+        nnn = xstr[idxx:].find(" ")
+        if nnn < 0:
+            print("bad encoding at ", xstr[idxx:idxx+5])
+        slen = int(xstr[idxx:idxx+nnn])
+        print("slen=", slen)
+        if slen >= len(xstr):
+            print("bad encoding at ", xstr[idxx:idxx+5])
+        idxx += nnn + 2
+        sval = str(xstr[idxx:idxx+slen])
+        #print("dict str:", "'" + sval + "'")
+        # iterate dict str
+        deco = self.decode_data(sval)
+        idxx += slen + 2
+        #print("idxx:", idxx, "var:", "{" + sval + "}", "next:", "'" + xstr[idxx:idxx+6] + "'")
+        ddd = {}
+        for aaa in deco[0]:
+            #print("aaa", aaa)
+            ddd[aaa[0]] = aaa[1]
+        return idxx, ddd
+
+    def __found_ext(self, xstr):
         idxx = 0
         #print ("found ext:", xstr)
         idxx = 1
@@ -210,7 +248,7 @@ class packbin():
         #print("idxx:", idxx, "var:", "{" + sval + "}", "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, sval
 
-    def _found_bin(self, xstr):
+    def __found_bin(self, xstr):
         idxx = 0
         #print ("found bin:", xstr)
         idxx = 1
@@ -229,7 +267,7 @@ class packbin():
         #print("idxx:", idxx, "var:", "{" + sval + "}", "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, deco
 
-    def _found_list(self, xstr):
+    def __found_list(self, xstr):
         idxx = 0
         #print ("found list:", xstr)
         idxx = 1
@@ -247,8 +285,26 @@ class packbin():
         #print("idxx:", idxx, "var:", "{" + sval + "}", "next:", "'" + xstr[idxx:idxx+6] + "'")
         return idxx, deco
 
+    def __found_tuple(self, xstr):
+        idxx = 0
+        #print ("found tuple:", xstr)
+        idxx = 1
+        nnn = xstr[idxx:].find(" ")
+        if nnn < 0:
+            print("bad encoding at ", xstr[idxx:idxx+5])
+        slen = int(xstr[idxx:idxx+nnn])
+        #print("slen", slen)
+        if slen >= len(xstr):
+            print("bad encoding at ", xstr[idxx:idxx+5])
+        idxx += nnn + 2
+        sval = str(xstr[idxx:idxx+slen])
+        deco = self.decode_data(sval)
+        idxx += slen + 2
+        #print("idxx:", idxx, "var:", "{" + sval + "}", "next:", "'" + xstr[idxx:idxx+6] + "'")
+        return idxx, tuple(deco)
+
     def _eval_one(self, dstr, idx2):
-        #print ("eval_one", dstr[idx2:])
+        #print ("  eval_one", dstr[idx2:])
         nstr = dstr[idx2:idx2+1]
         #print("nstr: ", "[" + nstr + "]")
         found = False; idx3 = 1; val = None
@@ -260,7 +316,8 @@ class packbin():
                 found = True
         if not found:
             # We do not raise an exception, rather inform the use
-            print("Warn: Invalid char in '%c' format string" % nstr)
+            #print("Warn: Invalid char in '%c' (at %d) format string in '%s" % (nstr, idx2, dstr))
+            print("Warn: Invalid char in '%c'" % nstr)
         return idx3, val
 
     # Estabilish a proper format string autmatically
@@ -301,11 +358,15 @@ class packbin():
                 aaa += "f"
             elif type(aa).__name__ == "list":
                 #print ("adding list")
+                aaa += "a"
+
+            elif type(aa).__name__ == "tuple":
+                #print ("adding tuple")
                 aaa += "t"
 
-            #elif type(aa).__name__ == "dict":
-            #    print ("adding dict")
-            #    aaa += "d"
+            elif type(aa).__name__ == "dict":
+                #print ("adding dict")
+                aaa += "d"
 
             else:
                 raise InvalidType( "Unsupported type: "  + str(type(aa).__name__ ))
@@ -313,30 +374,27 @@ class packbin():
         #print("autotype res", aaa)
         return aaa
 
-    ##########################################################################
-    # Encode data into a string
-    # Pass format string as the first element. Empty string switches on
-    # autotype
+    # Add front string
 
-    def encode_data(self, *formstr):
+    def __encode_data(self, front, *formstr):
 
-        if self.verbose > 4:
-            print("formatstr:", formstr[0])
-
-        if self.verbose > 3:
-            for aa in formstr:
-                print("got str:", aa)
+        #print("front", front, "formstr", formstr)
 
         localf = formstr[0]
         if  localf == "":
             localf = self.autotype(formstr[1:])
-            if self.verbose > 4:
-                print("Autotype '" + localf + "'");
+            if self.verbose > 6:
+                print("Autotype: '" + localf + "'");
+        else:
+            if self.verbose > 6:
+                print("formatstr:", formstr[0])
+                for aa in formstr:
+                    print("got format:", aa)
 
-        packed_str = "pg "
+        packed_str = front
 
         # Add the form string itself
-        packed_str += self._got_str("s", localf)
+        packed_str += self.__got_str("s", localf)
 
         idx = 1;
         for bb in localf:
@@ -345,7 +403,8 @@ class packbin():
             #print("bb", bb, end=" ")
             for cc in self.typeact:
                 if cc[0] == bb:
-                    #print ("found", cc[0], cc[1], formstr[idx])
+                    if self.verbose > 5:
+                        print ("found", cc[0], cc[1], formstr[idx])
                     if cc[1]:
                         packed_str += cc[1](bb, formstr[idx])
                     idx += 1
@@ -357,6 +416,15 @@ class packbin():
             raise ValueError("More data than chars in format string")
 
         return packed_str
+
+    ##########################################################################
+    # Encode data into a string
+    # Pass format string as the first element. Empty string switches on
+    # autotype
+
+    def encode_data(self, *formstr):
+        rrr = self.__encode_data("pg ", *formstr)
+        return rrr
 
     def decode_data(self, dstr):
 
--- common/testpacker.py
+++ common/testpacker.py
@@ -10,7 +10,6 @@ from Crypto.Hash import SHA512
 
 import support, pypacker
 
-
 # ------------------------------------------------------------------------
 # Test harness
 
@@ -20,51 +19,34 @@ if __name__ == '__main__':
             "/SxqZZxwyKc/ZVzCVwMxiJ5X8LdX3X5VVO5zq/VBWQ=="
 
     pb = pypacker.packbin();
-    pb.verbose = 0
-
-    '''
-    zorg = { 334 : "subx", 'x' : 123 }
-    xorg = ["val1", "val2", "val3"]
-    yorg = ["1", "2", "3"]
-    sorg_var = [ 334, "subx", 'x', xorg, yorg]
-    #sorg_var = [ 334, "subx", 'x', xorg, zorg]
-    if pb.verbose > 2:
-        print ("sorg_var:\n", sorg_var)
-    eee_var = pb.encode_data("", *sorg_var)
-    if pb.verbose > 2:
-        print ("eee_var type", type(eee_var).__name__, ":\n", eee_var)
-    fff_var = pb.decode_data(eee_var)
-    if pb.verbose > 2:
-        print ("fff_var:\n", fff_var)
-    if  sorg_var != fff_var:
-        print("Error on compare")
-    #sys.exit(0)
-    '''
+    pb.verbose = 5
 
     #bindat = Random.new().read(64)
     #print("bindat64:\n", base64.b64encode(bindat))
 
     bindat = base64.b64decode(rrr)
 
-    org = [ 33, "sub", 'd', "longer str here with \' and \" all crap",  33, 33333333.2, bindat ]
+    org = [ 33, "sub", 'd', "longer str here with \' and \" all crap",  33, 33333333.2,
+                {"test": 1111, "test2": 1112, }, bindat ]
 
     if pb.verbose > 2:
         print ("org:\n", org)
-    eee = pb.encode_data("", *org)
+
+    eeenc = pb.encode_data("", *org)
     if pb.verbose > 2:
-        print("eee:\n", "{" + eee + "}")
+        print("eeenc:\n", "{" + eeenc + "}")
 
-    #print ("eee", eee)
-
-    ddd = pb.decode_data(eee)
+    dddec = pb.decode_data(eeenc)
     if pb.verbose > 2:
-        print("ddd:\n",  "{" + str(ddd) + "}")
+        print("dddec:\n",  "{" + str(dddec) + "}")
 
-    if org == ddd:
+    if org == dddec:
         #print("Data matches OK.")
         pass
     else:
         print("MISMATCH:")
+
+    sys.exit(0)
 
     #print ("Should print 3 successes")
     #iscsifb
