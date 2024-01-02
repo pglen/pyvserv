24cc6ecc1ad94c2bfdcda2ea01bae60d1a294944
Peter Glen <peterglen@gmail.com>
2024-01-02 00:04:01 -0500

    auto

--- client/killserv.sh
+++ client/killserv.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#kill `cat ../server/.pysverv/lock` 2>/dev/null
-
-PID=`ps xa | grep "pyserv.py" | grep python |  awk {'printf "%03d\n", $0'}`
-
+PID=`ps xa | grep "pyvserv.py" | grep python |  awk {'printf "%03d\n", $0'}`
 if [ "$PID" != "" ] ; then
-kill $PID
+    kill $PID
 fi
 
 
